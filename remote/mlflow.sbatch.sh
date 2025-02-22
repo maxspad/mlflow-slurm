8702cd8d93593a72e4a49fbb49d991c5c5492528
Maxwell Spadafore <maxspad@Maxwells-MacBook-Pro.local>
2025-02-22 11:05:57 -0500

    using  now

--- remote/mlflow.sbatch.sh
+++ remote/mlflow.sbatch.sh
@@ -4,11 +4,14 @@
 #SBATCH --time=04:00:00
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=8G
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
 
 #conda activate conda/nlp-qual-um
 echo "Running on $(hostname)"
-srun -n 1 mlflow server -h 0.0.0.0 --backend-store-uri sqlite:///$SCRATCH/mlruns.db
+srun -n 1 mlflow server \
+            -h 0.0.0.0 \
+            --backend-store-uri sqlite:///$PROJDIR/mlruns.db \
+            --default-artifact-root $PROJDIR/mlartifacts
 
 
